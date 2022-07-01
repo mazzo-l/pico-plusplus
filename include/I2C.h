51aa0a28a510a00b41dabf577556d9a17b8e4791
mazzo-l <92888379+mazzo-l@users.noreply.github.com>
2022-07-01 20:28:41 +0200

    Minor syntax improvements

--- include/I2C.h
+++ include/I2C.h
@@ -5,13 +5,13 @@
 #include "Pin.h"
 
 class I2C {
-private:
-    i2c_inst* inst;
 public:
     I2C(int16_t slice, Pin scl, Pin sda, long baudrate = 40000);
     I2C() = default;
-    void write(uint8_t address, const uint8_t* src, size_t length, bool nostop);
-    void read(uint8_t address, uint8_t* dst, size_t length, bool nostop);
+    void write(uint8_t address, const uint8_t* src, size_t length, bool nostop) const;
+    void read(uint8_t address, uint8_t* dst, size_t length, bool nostop) const;
+private:
+    i2c_inst* inst{};
 };
 
 
